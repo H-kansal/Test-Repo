b2f9e850ef6dffd084207d9d37e0a9eda6b733e3
H-kansal <kansalhimanshu123@gmail.com>
2026-06-08 18:33:57 +0530

    4 commit

--- auth.py
+++ auth.py
@@ -3,11 +3,11 @@ SECRET_KEY = "super-secret-key"
 
 class UserManager:
     def login(self, username, password):
-        
+
         conn = sqlite3.connect("users.db")
 
         query = f"SELECT * FROM users WHERE username='{username}'"
-
+        
         result = conn.execute(query)
 
         for row in result:
